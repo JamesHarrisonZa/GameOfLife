15174daadda01d833a0ed0dacf9ac879e4dcfcc1
James Harrison <James.Harrison@Sage.com>
2016-12-28 18:49:23 +0200

    Got Basic map generator. Clicking and dragging over tiles changes colour

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+public class MapGenerator : MonoBehaviour
+{
+	// Properties available to Unity IDE
+
+	public Transform TilePrefab;
+	public Vector2 MapSize;
+	[Range(0,1)]
+	public float OutLinePercent;
+
+	// Private fields
+
+	const string HolderName = "Generated Map";
+
+	// Event Methods
+
+	void Start()
+	{
+		GenerateMap();
+	}
+
+	void Update()
+	{
+	}
+
+	// Private Methods
+
+	void GenerateMap()
+	{
+		if (transform.FindChild(HolderName))
+			DestroyImmediate(transform.FindChild(HolderName).gameObject);
+
+		var mapHolder = new GameObject(HolderName).transform;
+		mapHolder.parent = transform;
+
+		for (var x = 0; x < MapSize.x; x++)
+		{
+			for (var y = 0; y < MapSize.y; y++)
+			{
+				var tilePosition = new Vector3(-MapSize.x/2 + 0.5f + x, -MapSize.y / 2 + 0.5f + y, 0);
+				var newTile = Instantiate(TilePrefab, tilePosition, Quaternion.identity) as Transform;
+				newTile.localScale = Vector3.one*(1 - OutLinePercent);
+				newTile.parent = mapHolder;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tile : MonoBehaviour
+{
+	// Private fields
+
+	Renderer _renderer;
+
+	// Event Methods
+
+	void Start()
+	{
+		_renderer = GetComponent<Renderer>();
+		_renderer.material.color = Color.black;
+	}
+
+	void Update()
+	{
+		
+	}
+
+	void OnMouseDown()
+	{
+		ChangeColour();
+	}
+
+	void OnMouseEnter()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			ChangeColour();
+		}
+	}
+
+	// Private Methods
+
+	void ChangeColour()
+	{
+		_renderer.material.color = _renderer.material.color == Color.black ? Color.green : Color.black;
+	}
+}
